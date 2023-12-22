9012c937ed0ac5d7ca01a442eb65e3f8340205a4
trjohnson19 <77356759+trjohnson19@users.noreply.github.com>
2023-12-22 12:29:13 -0600

    Fix unpaired quote

--- appdata/redis/healthcheck.example.sh
+++ appdata/redis/healthcheck.example.sh
@@ -2,4 +2,4 @@
 
 # Replace <REDIS_PORT> and <REDIS_PASSWORD> with actual values
 # Do not forget to change permissions to be more restrictive
-redis-cli -p "<REDIS_PORT>" --pass "<REDIS_PASSWORD>" ping | grep PONG"
+redis-cli -p "<REDIS_PORT>" --pass "<REDIS_PASSWORD>" ping | grep "PONG"

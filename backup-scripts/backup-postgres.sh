b423a3fc2d3165a460e0c1cec16d0f6e23432ff3
trjohnson19 <77356759+trjohnson19@users.noreply.github.com>
2024-03-20 21:13:48 -0500

    Further SHFMT fixes

---
+++ backup-scripts/backup-postgres.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+function help_function() {
+	echo "Error parsing arguments"
+	echo ""
+	echo "Usage: $0 -u 'root' -p 'password' -d '/tmp' -f 'dump.sql'"
+	echo -e "\t-u Postgres root username. Default 'root'"
+	echo -e "\t-p Postgres root password. Default blank"
+	echo -e "\t-d Backup directory. Default '/tmp'"
+	echo -e "\t-f Backup filename. Default 'dump.sql'"
+	exit 1 # Exit script after printing help
+}
+
+while getopts u:p:d:f: flag
+do
+	case "${flag}" in
+	u) postgres_root_user="${OPTARG:-root}" ;;
+	p) postgres_root_password="${OPTARG}" ;;
+	d) backup_dir="${OPTARG:-/tmp}" ;;
+	f) backup_filename="${OPTARG:-dump.sql}" ;;
+	*) help_function ;;
+	esac
+done
+
+postgres_dump_params=(
+	--user="${postgres_root_user}"
+	--all-databases
+	--lock-all-tables
+	--verbose
+)
+
+if [[ -n $postgres_root_password ]]; then
+	postgres_dump_params+=(
+		--password="${postgres_root_password}"
+	)
+fi
+
+/usr/bin/docker exec --user root postgres /usr/bin/pg_dump \
+	"${postgres_dump_params[@]}" >"${backup_dir}/${backup_filename}"

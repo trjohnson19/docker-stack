61ee6ab284d2f010a8c7e586e0de67e57b541cf3
trjohnson19 <77356759+trjohnson19@users.noreply.github.com>
2024-03-20 21:46:52 -0500

    Fix default value logic, password logic

--- backup-scripts/backup-mariadb.sh
+++ backup-scripts/backup-mariadb.sh
@@ -3,23 +3,27 @@
 function help_function() {
 	echo "Error parsing arguments"
 	echo ""
-	echo "Usage: $0 -u 'root' -p 'password' -d '/tmp' -f 'dump.sql'"
+	echo "Usage: $0 -u 'root' -p 'password' -d '.' -f 'dump.sql'"
 	echo -e "\t-u MariaDB root username. Default 'root'"
-	echo -e "\t-p MariaDB root password. Default blank"
-	echo -e "\t-d Backup directory. Default '/tmp'"
+	echo -e "\t-p MariaDB root password. Default ''"
+	echo -e "\t-d Backup directory. Default \"\${PWD}\""
 	echo -e "\t-f Backup filename. Default 'dump.sql'"
-	exit 1 # Exit script after printing help
+	exit 1  # Exit script after printing help
 }
 
 while getopts u:p:d:f: flag; do
 	case "${flag}" in
-	u) mariadb_root_user="${OPTARG:-root}" ;;
+	u) mariadb_root_user="${OPTARG}" ;;
 	p) mariadb_root_password="${OPTARG}" ;;
-	d) backup_dir="${OPTARG:-/tmp}" ;;
-	f) backup_filename="${OPTARG:-dump.sql}" ;;
+	d) backup_dir="${OPTARG}" ;;
+	f) backup_filename="${OPTARG}" ;;
 	*) help_function ;;
 	esac
 done
+
+mariadb_root_user="${mariadb_root_user:-root}"
+backup_dir="${backup_dir:-${PWD}}"
+backup_filename="${backup_filename:-dump.sql}"
 
 mariadb_dump_params=(
 	--user="${mariadb_root_user}"
@@ -28,7 +32,7 @@ mariadb_dump_params=(
 	--verbose
 )
 
-if [[ -v "${mariadb_root_password}" ]]; then
+if [[ -v mariadb_root_password ]]; then
 	mariadb_dump_params+=(
 		--password="${mariadb_root_password}"
 	)
@@ -36,3 +40,7 @@ fi
 
 /usr/bin/docker exec --user root mariadb /usr/bin/mariadb-dump \
 	"${mariadb_dump_params[@]}" >"${backup_dir}/${backup_filename}"
+
+echo "MariaDB dump saved to: ${backup_dir}/${backup_filename}"
+
+exit 0
--- backup-scripts/backup-postgres.sh
+++ backup-scripts/backup-postgres.sh
@@ -3,30 +3,34 @@
 function help_function() {
 	echo "Error parsing arguments"
 	echo ""
-	echo "Usage: $0 -u 'root' -p 'password' -d '/tmp' -f 'dump.sql'"
+	echo "Usage: $0 -u 'root' -p 'password' -d '.' -f 'dump.sql'"
 	echo -e "\t-u Postgres root username. Default 'root'"
-	echo -e "\t-p Postgres root password. Default blank"
-	echo -e "\t-d Backup directory. Default '/tmp'"
+	echo -e "\t-p Postgres root password. Default ''"
+	echo -e "\t-d Backup directory. Default \"\${PWD}\""
 	echo -e "\t-f Backup filename. Default 'dump.sql'"
-	exit 1 # Exit script after printing help
+	exit 1  # Exit script after printing help
 }
 
 while getopts u:p:d:f: flag; do
 	case "${flag}" in
-	u) postgres_root_user="${OPTARG:-root}" ;;
+	u) postgres_root_user="${OPTARG}" ;;
 	p) postgres_root_password="${OPTARG}" ;;
-	d) backup_dir="${OPTARG:-/tmp}" ;;
-	f) backup_filename="${OPTARG:-dump.sql}" ;;
+	d) backup_dir="${OPTARG}" ;;
+	f) backup_filename="${OPTARG}" ;;
 	*) help_function ;;
 	esac
 done
+
+postgres_root_user="${postgres_root_user:-root}"
+backup_dir="${backup_dir:-${PWD}}"
+backup_filename="${backup_filename:-dump.sql}"
 
 postgres_dump_params=(
 	--username="${postgres_root_user}"
 	--verbose
 )
 
-if [[ -v "${postgres_root_password}" ]]; then
+if [[ -v postgres_root_password ]]; then
 	postgres_dump_params+=(
 		--password="${postgres_root_password}"
 	)
@@ -38,3 +42,7 @@ fi
 
 /usr/bin/docker exec --user root postgres /usr/bin/pg_dumpall \
 	"${postgres_dump_params[@]}" >"${backup_dir}/${backup_filename}"
+
+echo "Postgres dump saved to: ${backup_dir}/${backup_filename}"
+
+exit 0
